1de566c6e337ca9a69790bf7e2ce189c7e05beee
timmtonga <timmtonga@gmail.com>
2018-03-23 13:58:09 -0400

    Finalized transitions between patients

--- src/libs/application.js
+++ src/libs/application.js
@@ -1,7 +1,5 @@
-let state = {
-    patient: {},
-}
-let smart_local = {};
+var SMART;
+
 const filter = {
     date: (date, format) => {
     return (new XDate(date)).toString(format)
@@ -38,14 +36,14 @@ age: (date) => {
     let hours = (new Date().getTime() - new Date(date).getTime()) / 36e5
     if (dateAge > 1) hours = hours/(24 * dateAge)
 
-    if ( (yearAge > 0) && (monthAge > 0) && (dateAge > 0) ) return yearAge + "y " + monthAge + "m " + dateAge + "d"
-    else if ( (yearAge > 0) && (monthAge > 0) && (dateAge == 0) ) return yearAge + "y " + monthAge + "m"
-    else if ( (yearAge > 0) && (monthAge == 0) && (dateAge > 0) ) return yearAge + "y " + dateAge + "d"
-    else if ( (yearAge > 0) && (monthAge == 0) && (dateAge == 0) ) return yearAge + "y"
-    else if ( (yearAge == 0) && (monthAge > 0) && (dateAge > 0) ) return monthAge + "m " + dateAge + "d"
-    else if ( (yearAge == 0) && (monthAge > 0) && (dateAge == 0) ) return monthAge + "m"
-    else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 1) ) return dateAge + "d"
-    else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 0) ) return hours.toFixed(2) + "h"
+    if ( (yearAge > 0) && (monthAge > 0) && (dateAge > 0) ) return yearAge + " years " + monthAge + " months"
+    else if ( (yearAge > 0) && (monthAge > 0) && (dateAge == 0) ) return yearAge + " years " + monthAge + " months"
+    else if ( (yearAge > 0) && (monthAge == 0) && (dateAge > 0) ) return yearAge + " years " + dateAge + " days"
+    else if ( (yearAge > 0) && (monthAge == 0) && (dateAge == 0) ) return yearAge + " years"
+    else if ( (yearAge == 0) && (monthAge > 0) && (dateAge > 0) ) return monthAge + " months " + dateAge + " days"
+    else if ( (yearAge == 0) && (monthAge > 0) && (dateAge == 0) ) return monthAge + " months"
+    else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 1) ) return dateAge + " days"
+    else if ( (yearAge == 0) && (monthAge == 0) && (dateAge > 0) ) return hours.toFixed(2) + " hours"
     else return "Could not calculate age"
 },
 nameGivenFamily: (p) => {
@@ -73,123 +71,46 @@ nameGivenFamily: (p) => {
 },
 }
 
-function displayPatient(){
-    $('#name').text(state.patient.name)
-    $('#age').text(state.patient.name)
-    $('#gender').text(state.patient.sex)
+//This function displays a single patients demographic details
+function displayPatient(response){
+
+    var details = response.data // dealing with only the data portion of the response
+    var num_names = (details.name.length - 1)
+    var name = details.name[num_names].given.join(" ") + " " + details.name[num_names].family
+
+    $('#name').text(name)
+    $('#age').text((filter.age(details.birthDate)))
+    $('#gender').text(details.gender)
 }
 
-function queryPatient (smart) {
-    let deferred = $.Deferred()
-    $.when(smart.patient.read())
-        .done((patient) => {
-        state.patient.name = filter.nameGivenFamily(patient)
-
-    // Check for the patient-birthTime Extension
-    if (typeof patient.extension !== "undefined") {
-        patient.extension.forEach((extension) => {
-            if (extension.url === "http://hl7.org/fhir/StructureDefinition/patient-birthTime") {
-            state.patient.dob = extension.valueDateTime
-        }
-    })
-    }
-    // if dob wasn't set by the extension
-    if (state.patient.dob === undefined) state.patient.dob = patient.birthDate
-
-    state.patient.dob = new Date(state.patient.dob)
-    state.patient.sex = patient.gender
-    state.patient.id  = patient.id
-    deferred.resolve()
-})
-    return deferred
-}
-
-function getAllPatients() {
-
-    $.get( "https://api-v5-stu3.hspconsortium.org/dmDBMI/open/Patient/", function( data ) {
-        state.patients = data.entry;
-        displayPatients();
-    });
-}
-
-function displayPatients()
-{
-    for(i = 0; i < state.patients.length; i++)
-    {
-        num_names = (state.patients[i].resource.name.length - 1)
-        name = state.patients[i].resource.name[num_names].given.join(" ") + " " + state.patients[i].resource.name[num_names].family
-        gender = state.patients[i].resource.gender
-        age = (filter.age(state.patients[i].resource.birthDate))
-        $("#patientsTable > tbody").append("<tr><td>"+name+"</td><td>"+age+"</td><td>"+gender+"</td>" +
-            "<td style='text-align: center'><button class= 'btn btn-primary' onmousedown='showPatient("+ state.patients[i].resource.id +")'>View</button></td></tr>");
-    }
-}
-
+//Query to show one patients information
 function showPatient(id)
 {
-    console.log("Querying patient")
-    smart_local.patient.id = id
-    queryPatient(smart_local).done(() => {
-        displayPatient()
-    })
+    var patient = SMART.api.read({type: "Patient",id: id}).then(function(details){
+        displayPatient(details)
+    });
 }
 
-FHIR.oauth2.ready((smart) => {
-    smart_local = smart;
-    getAllPatients();
-    /*
-    queryPatient(smart).done(() => {
-        displayPatient()
-})*/
+//This function displays all the patients in the database in a table
+function displayPatients(patients)
+{
+
+    for(i = 0; i < patients.length; i++)
+    {
+        num_names = (patients[i].name.length - 1)
+        name = patients[i].name[num_names].given.join(" ") + " " + patients[i].name[num_names].family
+        gender = patients[i].gender
+        age = (filter.age(patients[i].birthDate))
+        $("#patientsTable > tbody").append("<tr><td>"+name+"</td><td>"+age+"</td><td>"+gender+"</td>" +
+            "<td style='text-align: center'><button class= 'btn btn-primary' style='width:70%;' onmousedown='showPatient("+ patients[i].id +")'>View</button></td></tr>");
+    }
+}
+
+//Execution begins here
+FHIR.oauth2.ready((fhirClient) => {
+    SMART = fhirClient;
+   fhirClient.api.fetchAll({ type: "Patient" }).then(function(patients) {
+        displayPatients(patients)
+
+    });
 })
-
-/*
-(function(){
-    'use strict';
-    var $ = jQuery;
-    $.fn.extend({
-        filterTable: function(){
-            return this.each(function(){
-                $(this).on('keyup', function(e){
-                    $('.filterTable_no_results').remove();
-                    var $this = $(this),
-                        search = $this.val().toLowerCase(),
-                        target = $this.attr('data-filters'),
-                        $target = $(target),
-                        $rows = $target.find('tbody tr');
-
-                    if(search == '') {
-                        $rows.show();
-                    } else {
-                        $rows.each(function(){
-                            var $this = $(this);
-                            $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
-                        })
-                        if($target.find('tbody tr:visible').size() === 0) {
-                            var col_count = $target.find('tr').first().find('td').size();
-                            var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
-                            $target.find('tbody').append(no_results);
-                        }
-                    }
-                });
-            });
-        }
-    });
-    $('[data-action="filter"]').filterTable();
-})(jQuery);
-
-$(function(){
-    // attach table filter plugin to inputs
-    $('[data-action="filter"]').filterTable();
-
-    $('.container').on('click', '.panel-heading span.filter', function(e){
-        var $this = $(this),
-            $panel = $this.parents('.panel');
-
-        $panel.find('.panel-body').slideToggle();
-        if($this.css('display') != 'none') {
-            $panel.find('.panel-body input').focus();
-        }
-    });
-
-})*/
